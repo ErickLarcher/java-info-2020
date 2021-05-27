3d717e76a148da3aba554bf8e11ad810129c087f
ErickLarcher <erik.larcher@gmail.com>
2021-05-27 08:31:39 -0300

    HolaMundo/HolaMundo.java

---
+++ HolaMundo/HolaMundo.java
@@ -0,0 +1,9 @@
+import jdk.javadoc.internal.doclets.formats.html.resources.standard;
+
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!");
+        System.out.println("Hola Informatorio JAVA!");
+    }
+}
