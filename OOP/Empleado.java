8842ab14d9244ddb2d1333ba40360a2850bbbc1e
ErickLarcher <erik.larcher@gmail.com>
2021-05-27 08:30:58 -0300

    HolaMundo

---
+++ OOP/Empleado.java
@@ -0,0 +1,19 @@
+public class Empleado {
+    int dni;
+    String nombre;
+    String apellido;
+    
+    public Empleado(int dni, String nombre, String apellido) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(this.dni) +
+            " - " + this.nombre +
+            " - " + this.apellido;
+    }
+    
+}
---
+++ OOP/ServicioSueldo.java
@@ -0,0 +1,9 @@
+public class ServicioSueldo {
+    
+    public static void main(String[] args) {
+        Empleado emp1 = new Empleado(123412, "Homero", "Simpson");
+        Empleado emp2 = new Empleado(123232412, "Robert", "Nelson");
+        System.out.println(emp1.toString());
+        System.out.println(emp2.toString());
+    }
+}
