26dcfaaf6f19b916e7bfe15dcb3420c6528dda3e
bluehotchilli <larcher.lds@gmail.com>
2021-06-08 22:22:14 -0300

    Ejercicios-Guia2/Ejerrcicio5.java

--- Ejercicios-Guia2/Ejerrcicio5.java
+++ Ejercicios-Guia2/Ejerrcicio5.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Ejerrcicio5 {
     
 
@@ -6,6 +10,28 @@ public class Ejerrcicio5 {
      contenga los totales y luego imprimir el total final a cobrar.*/
 
      public static void main(String[] args) {
+
+        /*Ambas listas pueden ser cargadas de forma iterativa con un ciclo for o while o usando su 
+        respectivo metodo "add" */
+
+        List<Integer> horasT = new ArrayList<>() {{ add(8); add(8); add(6); add(4); add(6);}};
+
+        List<Integer> valorPHora = new ArrayList<>() {{ add(200); add(457); add(154); add(364); add(333);}};
+
+        List<Integer> salario = new ArrayList<>();
+        int total = 0;
+
+
+        for(int i = 0 ; i < horasT.size(); i++){
+            salario.add((horasT.get(i) * valorPHora.get(i)));
+            total += salario.get(i);
+
+        }
+
+        System.out.println("Salario por semana: " + salario);
+        System.out.print("Total del salario: "+ total);
+        
+
 
      }
 }
